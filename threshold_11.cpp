37b6d8c5d8bdfa94c1708736f5f4be8fc9ab310c
ArunJayan <arunjayan32@gmail.com>
2015-11-06 19:40:30 +0530

    added more code

--- threshold_11.cpp
+++ threshold_11.cpp
@@ -2,12 +2,21 @@
 thresholding is the simplest segmentation metheod.
 
 There are 5 type of thresholding
-1. Binary
-2. Binary Inverted
-3. Threshold Truncated
-4. Threshold to zero
-5. Threshold to zero inverted
+0. Binary
+1. Binary Inverted
+2. Threshold Truncated
+3. Threshold to zero
+4. Threshold to zero inverted
 
+Binary thresholding (0) : if the pixel image[x,y] is higher than the threshold value , then the new pixel intensity is set to MaxVal
+					otherwise pixel are set to zero (image[x,y]<threshold value)
+Binary inverted(1) : 	opposite of Binary thresholding . 
+Threshold Trucate(2)  : if image[x,y]>threshold value then it is set to threshold
+						otherwise image[x,y]
+Threshold to zero (3): 	if image[x,y]<threshold value it is set zero
+						otherwise image[x,y]
+Threshold to zero inverted (4): if image[x,y]>threshold value , it set to 0
+							otherwise image[x,y]
 */
 //ref : http://docs.opencv.org/2.4/doc/tutorials/imgproc/threshold/threshold.html
 
@@ -23,17 +32,38 @@ int main(int argc,const char** argv)
 {
 	Mat img = imread("apple.jpg",CV_LOAD_IMAGE_COLOR);
 	//load the image
-	Mat gray,out;
+	Mat gray,binary_thresh,binary_inv,truncate_thresh,threshold2zero,threshold2zero_inv;
 	cvtColor(img,gray,CV_BGR2GRAY);
 	if(!img.data)
 	{
 		cout<<"Image Can't be loaded"<<endl;
 		return -1;
 	}
-	threshold(gray,out,90,255,1);
+	//threshold()
+	threshold(gray,binary_thresh,110,255,0); //binary thresholding 
+	//first argument : source image
+	//second argu 	 : output image
+	//third argu 	 : threshold value
+	//MaxVal 		 : The value used for binary thresholding operation
+	//threshold type : One of the 5 thresholding operation
+	threshold(gray,binary_inv,110,255,1);//binary inverted 
+	threshold(gray,truncate_thresh,110,255,2);//truncate
+	threshold(gray,threshold2zero,110,255,3);//threshold to zero
+	threshold(gray,threshold2zero_inv,110,255,4);
+	//windows
 	namedWindow("original",CV_WINDOW_NORMAL);
-	namedWindow("threshold",CV_WINDOW_NORMAL);
+	namedWindow("binar-threshold",CV_WINDOW_NORMAL);
+	namedWindow("binary_inv",CV_WINDOW_NORMAL);
+	namedWindow("truncate_thresh",CV_WINDOW_NORMAL);
+	namedWindow("threshold2zero",CV_WINDOW_NORMAL);
+	namedWindow("threshold2zero_inv",CV_WINDOW_NORMAL);
+
 	imshow("original",img);
-	imshow("threshold",out);
+	imshow("binar-threshold",binary_thresh);
+	imshow("binary_inv",binary_inv);
+	imshow("truncate_thresh",truncate_thresh);
+	imshow("threshold2zero",threshold2zero);
+	imshow("threshold2zero_inv",threshold2zero_inv);
 	waitKey(0);//wait  for keypress
+	destroyAllWindows();
 }
