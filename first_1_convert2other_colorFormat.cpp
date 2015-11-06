ce567d5600d3780f89fd52e436afab5cd08f2975
ArunJayan <arunjayan32@gmail.com>
2015-11-06 10:16:38 +0530

    modifoed

--- first_1_convert2other_colorFormat.cpp
+++ first_1_convert2other_colorFormat.cpp
@@ -1,4 +1,3 @@
-//HistogramEqualization_grayImage_08.cpp
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <iostream>
@@ -11,15 +10,23 @@ int main(int argc,const char** argv)
 	Mat img = imread("sample.png",CV_LOAD_IMAGE_COLOR);
 	//load the image
 	Mat gray;
-	if(!img.data)
+	if(!img.data)//if imageis not loaded
 	{
 		cout<<"Image Can not be loaded"<<endl;
 		return -1;
 	}
 	cvtColor(img,gray,CV_BGR2GRAY);
 	//cvtColor() can be ised convert image to other color formar like GRAYSCALE
-	//first input
-	namedWindow("gray",CV_WINDOW_AUTOSIZE);
-	imshow("gray",gray);
-	waitKey(0);
+	//first argument input is source imag
+	//second is target image
+	//third is target color format 
+	//available color format are :
+	// 				CV_BGR2GRAY
+	//				CV_BGR2HSV	
+	//				CV_HSV2BGR
+	//				CV_GRAY2BGR
+	//there is fourth parameter , if we have to change no of channels
+	namedWindow("gray",CV_WINDOW_AUTOSIZE);//creates a window
+	imshow("gray",gray);//show the image
+	waitKey(0);//wait for any keypress
 }
