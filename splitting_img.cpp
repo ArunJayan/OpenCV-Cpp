12513ff8957ab8cfb46d473371998524826d184b
ArunJayan <arunjayan32@gmail.com>
2015-11-12 21:58:09 +0530

    modified

--- splitting_img.cpp
+++ splitting_img.cpp
@@ -5,6 +5,7 @@ using namespace std;
 using namespace cv;
 
 Mat img; //image 
+vector<Mat> channels; // vector to store channels
 int main(int argc,const char** argv)
 {
 	img = imread("opencv_logo.png",CV_LOAD_IMAGE_COLOR);//load the image
@@ -13,5 +14,14 @@ int main(int argc,const char** argv)
 		cout<<"Image can't be loaded";
 	}
 
+	namedWindow("Splitting-Img",CV_WINDOW_AUTOSIZE);
+	//creates a window 
+	split(img,channels);
+	//split RGB image into channels : R,G,B
+	imshow("Splitting-Img",img);
+	imshow("B",channels[0]);
+	imshow("G",channels[1]);
+	imshow("R",channels[2]);
+	waitKey(0);
 	return 0;
 }
