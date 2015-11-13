61405fd8d2cea48682a3fe98e485680d3191a178
ArunJayan <arunjayan32@gmail.com>
2015-11-13 08:01:07 +0530

    Commenting done

--- mouseEvent_15.cpp
+++ mouseEvent_15.cpp
@@ -13,20 +13,20 @@ using namespace cv;
 
 void mouse_event_callback(int event,int x,int y,int flags,void*)
 {
-	if(event==EVENT_LBUTTONDOWN)
+	if(event==EVENT_LBUTTONDOWN)//if left button clicked
 	{
 		cout<<"\nLeft button of mouse is clicked Position:("<<x<<","<<y<<")";
 	}
-	else if (event==EVENT_RBUTTONDOWN)
+	else if (event==EVENT_RBUTTONDOWN)//if right button clicked
 	{
 		cout<<"\nRight button of mouse is clicked Position:("<<x<<","<<y<<")";
 	}
-	else if(event==EVENT_MBUTTONDOWN)
+	else if(event==EVENT_MBUTTONDOWN)//if middle button clicked
 	{
 		cout<<"\nMiddle button of mouse is clicked Position:("<<x<<","<<y<<")";
 
 	}
-	else if(event==EVENT_MOUSEMOVE)
+	else if(event==EVENT_MOUSEMOVE)//if mouse moved
 	{
 		cout<<"Mouse \nmove over the window Position:("<<x<<","<<y<<")";
 	}
@@ -34,14 +34,19 @@ void mouse_event_callback(int event,int x,int y,int flags,void*)
 int main(int argc,char** argv)
 {
 	Mat img = imread("sample.png",CV_LOAD_IMAGE_COLOR);
-	if(!img.data)
+	//load a image
+	if(!img.data)//if img can't be loaded exit with -1;
 	{
 		cout<<"Error in loading image";
 		return -1;
 	}
 	namedWindow("MouseEvent",CV_WINDOW_AUTOSIZE);
+	//creates a window named "MouseEvent"
 	setMouseCallback("MouseEvent",mouse_event_callback,NULL);
+	//set the callback function for any mouse event
 	imshow("MouseEvent",img);
+	//show the image
 	waitKey(0);
+	//wait for any keypress
 	return 0;
 }
