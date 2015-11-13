dfc137a4588affe397b9afa9292dc4648949890c
jkmj221 <jkmj007@gmail.com>
2015-11-13 11:59:33 +0530

    Matlab imtool alternative

--- imtool_16.cpp
+++ imtool_16.cpp
@@ -1,10 +1,53 @@
-#include <iostream>
+//imtool
+#include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <stdio.h>
+#include <iostream>
 
-using namespace std;
 using namespace cv;
+using namespace std;
 
-int main(int argc),const char** argv)
+Mat img;
+
+void imtool(int event, int x, int y, int flags, void* param)
 {
-	return 0;
+    char text[100];
+    Mat img2, img3;
+
+    img2 = img.clone();
+
+    if (event == EVENT_LBUTTONDOWN)//if left button clicked
+    {
+        Vec3b p = img2.at<Vec3b>(y,x);
+        sprintf(text, "R=%d, G=%d, B=%d", p[2], p[1], p[0]);
+    }
+    else if (event == EVENT_RBUTTONDOWN)
+    {
+        cvtColor(img, img3, CV_BGR2HSV);
+        Vec3b p = img3.at<Vec3b>(y,x);
+        sprintf(text, "H=%d, S=%d, V=%d", p[0], p[1], p[2]);
+    }
+    else
+    {
+
+     	 sprintf(text, "x=%d, y=%d", x, y);
+     	 //cout<<text;
+    }
+    cout<<text<<endl;
+    putText(img2, text, Point(430,15), FONT_HERSHEY_COMPLEX_SMALL, 0.8, Scalar(0,0,255));
+    imshow("imtool", img2);
+}
+
+int main(int argc, char** argv)
+{
+    img = imread("sample.png");
+    if (img.empty())
+        return -1;
+
+    namedWindow("imtool");
+    setMouseCallback("imtool", imtool, 0);
+    imshow("imtool", img);
+    waitKey(0);
+
+    return 0;
 }
