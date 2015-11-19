6abc8112192f20ee1a8132358577348702e60c26
ArunJayan <arunjayan32@gmail.com>
2015-11-19 14:23:50 +0530

    correct the errors

--- rotate_image_22.cpp
+++ rotate_image_22.cpp
@@ -3,24 +3,24 @@
 #include <iostream>
 
 using namespace std;
-using namespacecv;
+using namespace cv;
 
 int main(int argc,char** argv)
 {
 	//Load image 
-	Mat img = imread("sample.png")
+	Mat img = imread("sample.png");
 	if(!img.data)//otherwise quit 
 	{
 		cout<<"Image can not be loaded."<<endl;
 	}
 	namedWindow("Rotate",CV_WINDOW_AUTOSIZE);
 	//window created 
-	int angle = 0;
+	int angle = 180;
 	createTrackbar("Angle : ","Rotate",&angle,360);
 	//trackbar to variying the angle 
 	while(true)
 	{
-		Mat rotation = getRotationMatrix2D(Point(img.rows/2,img.cols/2),angle,1);
+		Mat rotation = getRotationMatrix2D(Point(img.rows/2,img.cols/2),angle-180,1);
 		//This function returns 2x3 affine transformation matrix for the 2D rotation.
 		//Aurguments : 
 		//			 Center : center of the rotation of the source img
@@ -35,7 +35,8 @@ int main(int argc,char** argv)
 		//			 3rd : Affine Transformation matrix
 		//			 4th : Size of destination image
 		imshow("Rotate",img_rotated);//rotated image 
-		if(waitKey(30)==27)
+		int key = waitKey(30);
+		if(key==27)
 		{
 			//if esc is prssed 
 			break;
