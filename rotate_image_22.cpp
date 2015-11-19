d724facf2512a0fd3378c406cec6b03f066130ba
ArunJayan <arunjayan32@gmail.com>
2015-11-19 14:00:25 +0530

    Rotating image

---
+++ rotate_image_22.cpp
@@ -0,0 +1,11 @@
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <iostream>
+
+using namespace std;
+using namespacecv;
+
+int main(int argc,char** argv)
+{
+	
+}
